c385ac9d29fac5fa63ef44a0e60e10797b3637c8
geraldofigueiredo <geraldo.figueiredosj@gmail.com>
2021-04-07 01:21:12 -0300

    chore: const values for animal type

---
+++ pkg/animal/animalType.go
@@ -0,0 +1,10 @@
+package animal
+
+type AnimalType string
+
+const (
+	MaleFeline   AnimalType = "FelinoMacho"
+	FemaleFeline AnimalType = "FelinaFemea"
+	CanineMale   AnimalType = "CaninoMacho"
+	CanineFemale AnimalType = "CanineFemale"
+)
