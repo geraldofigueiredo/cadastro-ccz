35cb110d6bec11ede6a9852e45b17b1935fd6756
geraldofigueiredo <geraldo.figueiredosj@gmail.com>
2021-04-08 21:25:04 -0300

    feat: aditional user informations to signup

---
+++ internal/domains/user/address.go
@@ -0,0 +1,11 @@
+package user
+
+type Address struct {
+	PostalCode   string
+	Street       string
+	Number       uint
+	Complement   string
+	Neighborhood string
+	City         string
+	State        string
+}
